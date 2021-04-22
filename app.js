a11ba9bfc18d4537bee7cc687f8a8796fb677e32
Messipsa <im_tamazouzt@esi.dz>
2021-04-22 09:42:00 +0200

    Schema

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+
 const app = express();
 
+// connect to mongo db
+const database = 'mongodb+srv://messipsa:272727@cluster0.od8qi.mongodb.net/node?retryWrites=true&w=majority';
+mongoose.connect(database , { useNewUrlParser : true , useUnifiedTopology : true})
+.then((result)=> app.listen(3000))
+.catch((err)=> console.log(err));
 // register view engine 
 
 app.set('view engine' , 'ejs');
 
-app.listen(3000);
+
+
+/*app.use((req,res ,next)=>
+{
+ console.log('new request :');
+ console.log(req.hostname);
+ console.log(req.path);
+ console.log(req.method);
+ next();
+});*/
+
+app.use(morgan('tiny'));
+
+app.use(express.static('public'));
 
 app.get('/' , (req , res)=>
 {
@@ -16,7 +38,8 @@ app.get('/' , (req , res)=>
     // res.sendFile('./views/index.html',{root:__dirname});
    
    const blogs = [
-       {title : "Jsk" , snippet : "LDC CAF 2010"},
+       {title : "Jsk" , snippet : "LDC CAF 2010 était très proche pour la remporter sauf que le destin a choisi un autre sort .\n"+
+       "La Jsk a deja éliminé le Club africain puis le Petro Atlético. Durant la phase de poules elle ecrase les deux Egyptiens Al Ahly et Al Ismailia ainsi que Hertland Fc avant de s'incliner face au Tp Mazembe."},
        {title : "FCB " , snippet : "sextuplé 2009"},
         {title : "Athletic Bilbao" , snippet : "Un Ogre Basque"},
    ];
